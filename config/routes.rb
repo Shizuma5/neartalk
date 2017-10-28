663a7b75b7c92cd1f1448476cab9e98b54aa44a1
Shizuma <kirua515@gmail.com>
2017-10-28 12:58:10 +0900

    ルーティングのルートをタイムラインに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root   'sessions#new'
+  root   'microposts#show'
   get    '/signup',  to: 'users#new'
   post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
