8c1780952e854dbee6b176bca32b436c3df1043d
Shizuma <kirua515@gmail.com>
2017-10-30 15:39:01 +0900

    micropostコントローラにnewアクションを追加し、投稿をするためのページを作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,8 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create, :destroy, :show]
+  before_action :logged_in_user, only: [:new, :create, :destroy, :show]
+
+  def new
+  end
 
   def show
     @user = current_user
